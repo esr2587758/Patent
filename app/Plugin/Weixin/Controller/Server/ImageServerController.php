231f2f245635ce855c26b33f0d57d2a59261662d
chinkei <chenjia@zhihuiya.com>
2014-01-27 13:28:57 +0800

    fixed news bug

--- app/Plugin/Weixin/Controller/Server/ImageServerController.php
+++ app/Plugin/Weixin/Controller/Server/ImageServerController.php
@@ -1,30 +1,25 @@
 <?php
 App::uses('WeixinServerController', 'Weixin.Controller');
 
-use PHPSDK\Response\News;
-
 class ImageServerController extends WeixinServerController {
 	
 	public function response() {
 		
-		// 微信请求参数
-		$requestData = $this->getWeixinRequestData();
-		// debug($requestData);
-		
-		$textResponse = new News(array(
-				'items' => array(
-					array(
-						'title' => '小样就你能发',
-						'description' => '有胆量你点我啊',
-						'pic_url' => 'http://img.funshion.com/pictures/536/625/536625.jpg',
-						'url' => 'http://www.innosnap.com/'
-					)
+		$this->responseXML(array(
+			'items' => array(
+				array(
+					'title' => '小样就你能发',
+					'description' => '有胆量你点我啊',
+					'pic_url' => 'http://img.funshion.com/pictures/536/625/536625.jpg',
+					'url' => 'http://www.innosnap.com/'
 				),
-				'fromUserName' => $this->getWeixinRequestData('tousername'),
-				'toUserName' => $this->getWeixinRequestData('fromusername'),
-		));
-		
-		$this->send($textResponse);
-		
+				array(
+					'title' => '再看',
+					'description' => '把你吃掉',
+					'pic_url' => 'http://img.funshion.com/pictures/536/625/536625.jpg',
+					'url' => 'http://www.innosnap.com/'
+				),
+			),
+		), 'news');
 	}
 }
